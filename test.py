41d78b00b571cd979f4c9d440dbb7412f80504d0
zeph1997 <zeph1997@hotmail.com>
2020-05-19 00:57:51 +0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,29 @@
+# An example of python variables and what they can do
+
+# Integers, float (decimals) can perform mathematical computations + - * / 
+my_integer = 1 + 2
+print(my_integer)
+
+# Strings are a string of characters. Do note that characters can include spaces and symbols
+my_string = "Hello World"
+print(my_string)
+
+# Strings can also be added together to make longer strings. But cannot be subtracted
+my_string2 = my_string + " this is the second part"
+print(my_string2)
+
+# Lists consists of many items
+my_list = [3,4,90,100]
+print(my_list)
+
+# Call only one element. Take note the elements start from 0
+# Call out the number 90!
+print(my_list[])
+
+# Split long texts into lists
+my_variable = "Part Of Many Words"
+my_variable = my_variable.split(" ")
+print(my_variable)
+
+#print out the word "Of"
+print(my_variable[])
