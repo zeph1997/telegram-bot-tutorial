4dbcfb696de95a85b22cc25f64bf49b6f34730b0
zeph1997 <zeph1997@hotmail.com>
2020-05-19 01:13:01 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,29 +1,63 @@
-# An example of python variables and what they can do
+## An example of python variables and what they can do
 
-# Integers, float (decimals) can perform mathematical computations + - * / 
+## Integers, float (decimals) can perform mathematical computations + - * / 
 my_integer = 1 + 2
 print(my_integer)
 
-# Strings are a string of characters. Do note that characters can include spaces and symbols
-my_string = "Hello World"
-print(my_string)
+## Strings are a string of characters. Do note that characters can include spaces and symbols
+# my_string = "Hello World"
+# print(my_string)
 
-# Strings can also be added together to make longer strings. But cannot be subtracted
-my_string2 = my_string + " this is the second part"
-print(my_string2)
+## Strings can also be added together to make longer strings. But cannot be subtracted
+# my_string2 = my_string + " this is the second part"
+# print(my_string2)
 
-# Lists consists of many items
-my_list = [3,4,90,100]
-print(my_list)
+## Lists consists of many items
+# my_list = [3,4,90,100]
+# print(my_list)
 
-# Call only one element. Take note the elements start from 0
-# Call out the number 90!
-print(my_list[])
+## Call only one element. Take note the elements start from 0
+## Call out the number 90!
+# print(my_list[])
 
-# Split long texts into lists
-my_variable = "Part Of Many Words"
-my_variable = my_variable.split(" ")
-print(my_variable)
+## Split long texts into lists
+# my_variable = "Part Of Many Words"
+# my_variable = my_variable.split(" ")
+# print(my_variable)
 
-#print out the word "Of"
-print(my_variable[])
+## Print out the word "Of"
+# print(my_variable[])
+
+
+## If else conditions
+# if True == True:
+#    print("This evaluates to True")
+# else:
+#    print("This evaluates to False")
+
+## For loops
+# for i in range(0,10,1):
+#     print(i)
+#     i = i + 2
+#     print(i)
+#
+# for j in my_list:
+#     print(j)
+
+## While loop
+# while True == True:
+#     print("Hello")
+
+
+## Functions
+# def function_name(num1,num2,num3):
+#     send_back = num1 + num2 + num3
+#     return send_back
+
+# a = 5
+# b = 9
+# c = 11
+
+# get_from_function = function_name(a,b,c)
+
+# print(get_from_function)
