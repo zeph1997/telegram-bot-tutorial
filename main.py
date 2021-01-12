d321ff42d53db9d4ad9e90cb41dc76f62f6cc62e
zeph1997 <zeph1997@hotmail.com>
2021-01-12 14:11:06 +0800

    Update main.py

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ def get_price(message):
     bot.send_message(message.chat.id,"Getting prices...")
     for stock in stocks:
         url = f""       #add website URL here
-        response = requests.get(url)
+        response = requests.get(url, headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"})
         soup = BeautifulSoup(response.content, "html.parser")
         price = soup.select("")[0].text.strip()     #add the element that contains the stock price here
         timing = soup.select("")[0].text.strip()    #add the element that contains the time here
